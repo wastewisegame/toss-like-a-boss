9e093593f6f6c99c4ac1a555f6e251d33861fbab
Lukas Nord <luke.nord@gmail.com>
2020-05-17 22:22:00 -0500

    update configs for local usage of database. Update redux to use redux-dev tools.

---
+++ build/precache-manifest.640db0ad3cb49637fdaa9fe3dd6bc522.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f37e96733704b52639d649ebd7c36e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8bb44e15849b628e0f07",
+    "url": "/static/css/2.47271137.chunk.css"
+  },
+  {
+    "revision": "effdaa61b1d9743a86d5",
+    "url": "/static/css/main.d979cc09.chunk.css"
+  },
+  {
+    "revision": "8bb44e15849b628e0f07",
+    "url": "/static/js/2.fff66e55.chunk.js"
+  },
+  {
+    "revision": "b73ca391be51a086d6a055b65471bc88",
+    "url": "/static/js/2.fff66e55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "effdaa61b1d9743a86d5",
+    "url": "/static/js/main.c757ab73.chunk.js"
+  },
+  {
+    "revision": "d4d736e7226e1db5b904",
+    "url": "/static/js/runtime-main.7e26b5fb.js"
+  },
+  {
+    "revision": "49fafb302421193937187e7f6957d03b",
+    "url": "/static/media/KeeponTruckin.49fafb30.ttf"
+  },
+  {
+    "revision": "aed569a3687606eea7cf97438c00fdd6",
+    "url": "/static/media/Lake.aed569a3.JPG"
+  },
+  {
+    "revision": "e6689338c0447a1fa9073acf0814ae6a",
+    "url": "/static/media/WasteWiselogo.e6689338.jpg"
+  },
+  {
+    "revision": "7e78fcdf3d355815117d8d68a23bc80e",
+    "url": "/static/media/chunk.7e78fcdf.otf"
+  },
+  {
+    "revision": "4eae8aab0e442b4a55e312b5c013b6a2",
+    "url": "/static/media/fifthHowToSlide.4eae8aab.png"
+  },
+  {
+    "revision": "78384fb0be46d39229e1108c60780866",
+    "url": "/static/media/firstHowToSlide.78384fb0.png"
+  },
+  {
+    "revision": "d187aed80989b815c97194f2ab56d995",
+    "url": "/static/media/fourthHowToSlide.d187aed8.png"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "/static/media/roboto-latin-100.5cb7edfc.woff"
+  },
+  {
+    "revision": "7370c3679472e9560965ff48a4399d0b",
+    "url": "/static/media/roboto-latin-100.7370c367.woff2"
+  },
+  {
+    "revision": "f8b1df51ba843179fa1cc9b53d58127a",
+    "url": "/static/media/roboto-latin-100italic.f8b1df51.woff2"
+  },
+  {
+    "revision": "f9e8e590b4e0f1ff83469bb2a55b8488",
+    "url": "/static/media/roboto-latin-100italic.f9e8e590.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/static/media/roboto-latin-300.b00849e0.woff"
+  },
+  {
+    "revision": "ef7c6637c68f269a882e73bcb57a7f6a",
+    "url": "/static/media/roboto-latin-300.ef7c6637.woff2"
+  },
+  {
+    "revision": "14286f3ba79c6627433572dfa925202e",
+    "url": "/static/media/roboto-latin-300italic.14286f3b.woff2"
+  },
+  {
+    "revision": "4df32891a5f2f98a363314f595482e08",
+    "url": "/static/media/roboto-latin-300italic.4df32891.woff"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/static/media/roboto-latin-400.479970ff.woff2"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/static/media/roboto-latin-400.60fa3c06.woff"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/static/media/roboto-latin-400italic.51521a2a.woff2"
+  },
+  {
+    "revision": "fe65b8335ee19dd944289f9ed3178c78",
+    "url": "/static/media/roboto-latin-400italic.fe65b833.woff"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/static/media/roboto-latin-500.020c97dc.woff2"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/static/media/roboto-latin-500.87284894.woff"
+  },
+  {
+    "revision": "288ad9c6e8b43cf02443a1f499bdf67e",
+    "url": "/static/media/roboto-latin-500italic.288ad9c6.woff"
+  },
+  {
+    "revision": "db4a2a231f52e497c0191e8966b0ee58",
+    "url": "/static/media/roboto-latin-500italic.db4a2a23.woff2"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/static/media/roboto-latin-700.2735a3a6.woff2"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/static/media/roboto-latin-700.adcde98f.woff"
+  },
+  {
+    "revision": "81f57861ed4ac74741f5671e1dff2fd9",
+    "url": "/static/media/roboto-latin-700italic.81f57861.woff"
+  },
+  {
+    "revision": "da0e717829e033a69dec97f1e155ae42",
+    "url": "/static/media/roboto-latin-700italic.da0e7178.woff2"
+  },
+  {
+    "revision": "9b3766ef4a402ad3fdeef7501a456512",
+    "url": "/static/media/roboto-latin-900.9b3766ef.woff2"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "/static/media/roboto-latin-900.bb1e4dc6.woff"
+  },
+  {
+    "revision": "28f9151055c950874d2c6803a39b425b",
+    "url": "/static/media/roboto-latin-900italic.28f91510.woff"
+  },
+  {
+    "revision": "ebf6d1640ccddb99fb49f73c052c55a8",
+    "url": "/static/media/roboto-latin-900italic.ebf6d164.woff2"
+  },
+  {
+    "revision": "a8e5f4531051cc0cb621d51e875775c0",
+    "url": "/static/media/scoreboardfont.a8e5f453.ttf"
+  },
+  {
+    "revision": "0c080d9164fa75987b63135e7012a9cf",
+    "url": "/static/media/secondHowToSlide.0c080d91.png"
+  },
+  {
+    "revision": "e59638b2b49ec9944f8fd255cdd89b71",
+    "url": "/static/media/thirdHowToSlide.e59638b2.png"
+  }
+]);
