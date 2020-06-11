0b83e2d5320497b98aad9d3eb16b385fb38ea260
Lukas Nord <luke.nord@gmail.com>
2020-06-11 15:13:12 -0500

    fix import statement for material-ui

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,32 +1,27 @@
-let app = require('../server.js');
-let testServer = require('supertest');
+let app = require('../server.js')
+let testServer = require('supertest')
 
 describe('Test the route path', () => {
-    test('should respond 200 to /logout',  async () => {
+    test('should respond 200 to /logout', async () => {
         let response = await testServer(app).post('/api/user/logout')
-        expect(response.statusCode).toBe(200);
+        expect(response.statusCode).toBe(200)
     })
 
     test('should respond 403 to not logged ')
 
-
-
-
-
-
     test('/user should return user info when authenticated', async () => {
-        //Login 
+        //Login
         // agent remembers cookies
-        let agent = testServer.agent(app);
-        const response = await agent.post('/api/user/login')
-                                    // send data
-                                    .send({username: 'miles', password: '1234'})
-        expect(response.statusCode).toBe(200);
+        let agent = testServer.agent(app)
+        const response = await agent
+            .post('/api/user/login')
+            // send data
+            .send({ username: 'miles', password: '1234' })
+        expect(response.statusCode).toBe(200)
         // GET to /user
-        const userResponse = await agent.get('/api/user');
-        expect(userResponse.statusCode).toBe(200); 
+        const userResponse = await agent.get('/api/user')
+        expect(userResponse.statusCode).toBe(200)
     })
 })
 
-
-
+git
