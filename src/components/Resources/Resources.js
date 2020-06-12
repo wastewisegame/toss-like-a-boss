cdcd7e121e597dbabb7b4a8d4c5df4bcc7e9e20b
Lukas Nord <luke.nord@gmail.com>
2020-06-12 08:57:57 -0500

    cleaning up some unused vars

--- src/components/Resources/Resources.js
+++ src/components/Resources/Resources.js
@@ -7,8 +7,8 @@ import { withStyles } from '@material-ui/core/styles'
 import { Grid } from '@material-ui/core'
 import Button from '@material-ui/core/Button'
 import Paper from '@material-ui/core/Paper'
-import Card from '@material-ui/core/Card'
-import CardMedia from '@material-ui/core/CardMedia'
+//import Card from '@material-ui/core/Card'
+//import CardMedia from '@material-ui/core/CardMedia'
 
 const styles = {
     link: {
